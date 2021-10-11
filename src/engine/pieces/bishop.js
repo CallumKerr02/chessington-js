38cdce6adcafb87c5525d68aadac655752bdfaab
CallumKerr02 <callumkerr3@hotmail.com>
2021-10-11 10:42:44 +0100

    Cherry pick 4 completed.

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,11 +1,28 @@
 import Piece from './piece';
-
+import Square from '../square';
 export default class Bishop extends Piece {
     constructor(player) {
         super(player);
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
-    }
+        const currentSquare = board.findPiece(this);
+        const availableMovesOnBoard = [];
+        for (let i = 1; i <= 6; i++){
+            if (i !== -1 && currentSquare.row + i <= 7 && currentSquare.row + i >= 0 && currentSquare.col - i >= 0 && currentSquare.col -i <= 7){
+                availableMovesOnBoard.push(Square.at(currentSquare.row + i, currentSquare.col - i))
+            }  
+            if (i !== -1 && currentSquare.row - i >= 0 && currentSquare.row - i <= 7 && currentSquare.col + i >= 0 && currentSquare.col + i <= 7){
+                availableMovesOnBoard.push(Square.at(currentSquare.row - i, currentSquare.col + i))
+            }
+            if (i !== -1 && currentSquare.row + i >= 0 && currentSquare.row + i <= 7 && currentSquare.col + i >= 0 && currentSquare.col + i <= 7){
+                availableMovesOnBoard.push(Square.at(currentSquare.row + i, currentSquare.col + i))
+            }
+            if (i !== -1 && currentSquare.row - i >= 0 && currentSquare.row - i <= 7 && currentSquare.col - i >= 0 && currentSquare.col -i <= 7){
+                availableMovesOnBoard.push(Square.at(currentSquare.row - i, currentSquare.col - i))
+            }
+        }
+        return availableMovesOnBoard;
+    } 
 }
+ 
